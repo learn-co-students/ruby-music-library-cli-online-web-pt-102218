18b52e0eecfe2805e3f8963e33d0f200bdc28608
Linda Chang <lindachang186@gmail.com>
2018-12-05 20:01:46 +0000

    Done.

---
+++ lib/002_artist_basics_spec.rb
@@ -0,0 +1,22 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+end
