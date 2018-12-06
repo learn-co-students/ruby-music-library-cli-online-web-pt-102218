78d4eb51cc5d8d8f6935b3bc88c6638be5fb5d06
Linda Chang <lindachang186@gmail.com>
2018-12-06 21:15:09 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,25 @@
 require 'pry'
 
 class Song
-  attr_accessor :name, :artist
+  attr_accessor :name, :artist, :genre
 
 
   @@all = []
 
-  def initialize(name, artist= nil)
+  def initialize(name, artist= nil, genre= nil)
     @name = name
     self.artist=(artist) unless artist == nil
+    self.genre=(genre) unless genre == nil
   end
 
   def artist=(artist)
     @artist = artist
     artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self if !genre.songs.include?(self)
   end
 
   def save
