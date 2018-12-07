251c4fffb850fa4ca457712a001bd725ff821fc1
grant smith <grantsmith1256@gmail.com>
2018-12-07 03:19:04 +0000

    Done.

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -1,7 +1,30 @@
 class Artist
   
+  attr_accessor :name
   
+  @@all =[]
   
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
   
+ def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(name)
+    name = self.new(name)
+    name.save
+    name
+  end
   
 end
