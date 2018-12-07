3f0de1e19ebd3d2c9af160d80817539112bac8f2
Johnathon Wood <jaywood128@gmail.com>
2018-12-07 20:30:37 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -38,18 +38,16 @@ attr_accessor :name, :artist, :genre
 
   def self.create(name)
     song = Song.new(name)
-    @@all << song
-    song
   end
 
   def self.find_by_name(name)
 
-    @@all.find {|song| song.name == name }
+    @@all.detect {|song| song.name == name }
     #find will return nil of the name isn't in @@all
 
   end
 
   def self.find_or_create_by_name(name)
-    find_by_name(name) || create(name)  #if find_by_name(name) returns nil, the right side is executed
+    self.find_by_name(name) || self.create(name)  #if find_by_name(name) returns nil, the right side is executed
   end
 end
