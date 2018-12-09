46f8253ded2e7f0ea9f8d56506023a28bfcba68a
Johnathon Wood <jaywood128@gmail.com>
2018-12-09 19:58:50 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -1,24 +1,31 @@
+require 'pry'
 class MusicLibraryController
 
   def initialize(path = './db/mp3s')
     @path = path
-    @importer = MusicImporter.new(path).import
+    MusicImporter.new(path).import
   end
 
   def call
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    gets.chomp
+    user_input = " "
+    while user_input != "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      user_input = gets.strip
+     end
+    end
 
+      def list_songs
+        Song.all.sort_by {|song| song.name}.each.with_index(1) do |song, index|
+          puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
 
-  end
-
-
-end
+        end
+      end
+    end
