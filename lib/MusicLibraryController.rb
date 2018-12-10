0f081a3c65dc8f7fbd88d0b1e69cb2e145586595
grant smith <grantsmith1256@gmail.com>
2018-12-10 17:26:56 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -9,27 +9,32 @@ class MusicLibraryController
   
   def call
     input = ""
-    while input != "exit!"
+    while input != "exit"
     puts "Welcome to your music library!"
     puts "What would you like to do?"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
     input = gets.strip
-    case input
-     when "list songs"
-      songs
-     when "list artists"
-      artists
+    case(input)
+     when 'list songs'
+      list_songs
      when "list genres"
-      genres
-     when "list artist"
+      list_genres
+     when "list artists"
        list_artists
+     when "list artist"
+       list_songs_by_artist
      when "list genre"
-       list_genres
-       when "list song"
-         list_songs
-      when "play song"
-        play_song
-      end
-    end
+       list_songs_by_genre
+     when "play song"
+       play_song
+     end
+   end
   end
   
   def list_songs
