d0643245ac612f87a1c59481d016d3d5c33f9bc3
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-12-17 01:36:58 +0000

    testing

---
+++ lib/Artist.rb
@@ -0,0 +1,49 @@
+require 'pry'
+class Artist 
+  
+  extend Concerns::Findable 
+  
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.destroy_all 
+    @@all.clear 
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.create(name)
+    new = Artist.new(name)
+    new.save 
+    new 
+  end 
+  
+  def songs 
+    @songs 
+  end 
+  
+  def add_song(song)
+    song.artist = self unless song.artist 
+    @songs << song unless @songs.include?(song)
+  end 
+  
+  def genres
+    new = @songs.map do |i| 
+      i.genre 
+    end 
+    new.uniq
+  end 
+  
+end 
+  
---
+++ lib/MusicImporter.rb
@@ -0,0 +1,29 @@
+require 'pry'
+class MusicImporter 
+  
+  attr_accessor :path, :name
+  
+  def initialize(path)
+    @path = path 
+  end 
+  
+  def files
+    @new = []
+    dir = Dir.entries("./spec/fixtures/mp3s")
+    dir.each do |i| 
+      if i.split('').last(4) == '.mp3'.split('')
+        @new << i 
+      end 
+    end 
+    @new
+  end 
+  
+  def import 
+    self.files.each do |i| 
+      Song.create_from_filename(i)
+    end 
+  end 
+  
+  
+  
+end 
