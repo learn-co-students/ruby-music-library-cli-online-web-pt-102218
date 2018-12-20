2db1ba04f68599ed2f0cbfc2fef76fd78150b293
tatiana.mojica <tatiana.mojica@bustle.com>
2018-12-20 02:29:50 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,69 @@
+require "pry"
+# require "music_importer.rb"
+
+class Song 
+  
+  extend Concerns::Findable
+  attr_accessor :name, :artist, :genre
+  
+  @@all = []
+  
+  def initialize(name, artist= nil, genre= nil) 
+    @name = name
+    self.artist=(artist) unless artist == nil
+    # invokes artist method instead of simply assigning to an artist instance variable to ensure assocaiations are created upon initialization
+    self.genre=(genre) unless genre == nil
+    self.save
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.destroy_all 
+    @@all.clear
+  end 
+  
+  def self.create(name)
+    new_song = self.new(name)
+    new_song.save 
+    new_song
+    # new(name, artist, genre).tap{|s| s.save}
+  end 
+  
+  
+  def artist=(artist)
+    @artist = artist 
+    artist.add_song(self)
+  end 
+  
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end 
+  
+  def self.find_by_name(name) 
+    self.all.detect {|song|song.name == name} 
+    #you need to put name in as the arg. because you did that in initialize 
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name) 
+  end 
+  
+  def self.new_from_filename(filename)
+    filename_array = filename.split(" - ")
+    
+    song_artist = Artist.find_or_create_by_name(filename_array[0])
+    song_genre = Genre.find_or_create_by_name(filename_array[2].chomp(".mp3"))
+    Song.new(filename_array[1], song_artist, song_genre)
+  end 
+  
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename)
+  end 
+end 
--- spec/010_music_library_controller_spec.rb
+++ spec/010_music_library_controller_spec.rb
@@ -35,7 +35,7 @@ describe "MusicLibraryController" do
       allow(music_library_controller).to receive(:gets).and_return("exit")
 
       expect($stdout).to receive(:puts).with("Welcome to your music library!")
-      expect($stdout).to receive(:puts).with("To list all of your songs, enter 'list songs'.")
+      
       expect($stdout).to receive(:puts).with("To list all of the artists in your library, enter 'list artists'.")
       expect($stdout).to receive(:puts).with("To list all of the genres in your library, enter 'list genres'.")
       expect($stdout).to receive(:puts).with("To list all of the songs by a particular artist, enter 'list artist'.")
@@ -43,6 +43,7 @@ describe "MusicLibraryController" do
       expect($stdout).to receive(:puts).with("To play a song, enter 'play song'.")
       expect($stdout).to receive(:puts).with("To quit, type 'exit'.")
       expect($stdout).to receive(:puts).with("What would you like to do?")
+      expect($stdout).to receive(:puts).with("To list all of your songs, enter 'list songs'.")
 
       music_library_controller.call
     end
