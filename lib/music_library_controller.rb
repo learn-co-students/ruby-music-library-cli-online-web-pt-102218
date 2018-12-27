512b71052f4cabbe1b20c35bbdc7ea4159a3c014
Elyse Burmester <burmesterelyse@gmail.com>
2018-12-27 18:47:45 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -24,9 +24,13 @@ class MusicLibraryController
     gets
   end
   
+  #CLI Methods
   
   def list_songs
-  
+    list = Song.all.sort_by! { |a| a.name }
+    new = list.uniq.each.with_index(1) do |index, value|
+      puts "#{value}. #{index.artist.name} - #{index.name} - #{index.genre.name}"
+    end
   end
     
   
@@ -45,15 +49,51 @@ class MusicLibraryController
   end
   
   def list_songs_by_artist
-    puts "Please enter the name of an artist:"
-    input = gets.chomp
-    if Artist.all.include?!(input)
-      puts "Please enter the name of an artist:"
+        puts "Please enter the name of an artist:"
+        user_input = gets.strip
+        #when user inputs artist, return numbered(alphebatized) list of artist and genre
+      if artist = Artist.find_by_name(user_input)
+        artist.songs.sort_by {|artist_name| artist_name.name}.each_with_index do |song, index|
+          puts "#{index+1}. #{song.name} - #{song.genre.name}"
+        end
+      else
+    end
+  end
+   
+  def list_songs_by_genre
+     puts "Please enter the name of a genre:"
+        user_input = gets.strip
+      if genre = Genre.find_by_name(user_input)
+        genre.songs.sort_by {|genre_name| genre_name.name}.each_with_index do |song, index|
+          puts "#{index+1}. #{song.artist.name} - #{song.name}"
+        end
+      else
+    end
+  end
+  
+  def play_song
+    puts "Which song number would you like to play?"
+    input = gets.strip 
+    x = Integer(input) rescue false
+    if x.is_a?(Integer) == false 
+      nil 
+   elsif x <= Song.all.size 
+      Song.all.sort_by {|name| name.name}.each.with_index(1) do |index, value|
+        if value == x 
+          puts "Playing #{index.name} by #{index.artist.name}"
+        end
+      end
     else
-      # list = Song.all.sort_by! { |a| a.name }
-      # new = list.uniq.each.with_index(1) do |index, value|
-      #   puts "#{value}. #{index.name}"
-     end
-   end
+    end
+  end
+  
+  #CLI Commands
+  
+  def 'list songs'
+    MusicLibraryController.list_songs
+  end
+  
+  
+  
   
 end
